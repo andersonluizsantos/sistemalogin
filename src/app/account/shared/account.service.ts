2843efd9f4b381e9b55f38662eeb640a11cda67e
Anderson <andersonluizsantos@gmail.com>
2020-09-15 10:55:58 -0300

    Acessando a API de Carros para obter o token e testar o acesso ao sistema

--- src/app/account/shared/account.service.ts
+++ src/app/account/shared/account.service.ts
@@ -1,18 +1,28 @@
 import { Injectable, ResolvedReflectiveFactory } from '@angular/core';
 import { resolve } from 'url';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  login(user: any){
-    return new Promise((resolve) => {
+  async login(user: any){
+    const result = await this.http.post<any>(`${environment.api}/api/signin`, user).toPromise();
+    if (result && result.access_token) {
+      window.localStorage.setItem('token', result.access_token);
+      return true;
+    }
+    return false;
+    
+    /*return new Promise((resolve) => {
       window.localStorage.setItem('token', 'meu-token');
       resolve(true);
     });
+    */
   }
 
   createAccount(account: any){
